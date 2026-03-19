8e7c905a7e65fb338175ca396f4fb1c2e7b85428
Lokesh_Gundoju <216749630+LKSH-GNDJ@users.noreply.github.com>
2026-03-19 12:39:14 +0530

    Delete start_bot.bat

--- start_bot.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start cmd /k "venv\Scripts\activate && python -m backend.api"
-timeout /t 5
-start cmd /k "venv\Scripts\activate && python -m streamlit run frontend/app.py --server.headless true"
