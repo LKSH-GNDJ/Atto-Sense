61014d21b247f642af24ee8fad147639916214d7
Lokesh_Gundoju <216749630+LKSH-GNDJ@users.noreply.github.com>
2026-03-19 12:37:06 +0530

    Delete frontend_react directory

--- frontend_react/start_react.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies (first run only)...
-call npm install
-echo.
-echo Starting AttoSense React UI on http://localhost:3000
-echo Make sure the FastAPI backend is running on http://localhost:8000
-echo.
-call npm run dev
-pause
